b6bc9e60cd5ffacf177c226993632f163e6a6e5f
Tuguldur Sukhbold <tuguldur.s@gmail.com>
2022-08-02 02:04:39 -0400

    gooey

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(
     packages=['deidcm'],
     install_requires=[
         'tqdm>=4.62',
-        'pydicom==2.3.0'
+        'pydicom==2.3.0',
+        'pyinstaller>=4.8',
+        'gooey>=1.0.8'
     ],
     python_requires='>=3.9',
 )
