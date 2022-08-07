837dc5128338f4618820b9e2145b7656ca1009ea
Tuguldur Sukhbold <tuguldur.s@gmail.com>
2022-08-07 19:20:45 -0400

    hang fix

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ from gooey import GooeyParser
 from deidcm.utils import parse_log_config
 from deidcm.deidentifier import Deidentifier
 
+####### FIX ######
+# following redirects stdout to stderr
+# and prevents GUI hanging
+# see https://github.com/chriskiehl/Gooey/issues/520
+import sys
+import codecs
+
+if sys.stdout.encoding != 'UTF-8':
+    sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer, 'strict')
+if sys.stderr.encoding != 'UTF-8':
+    sys.stderr = codecs.getwriter('utf-8')(sys.stderr.buffer, 'strict')
+##################
+
 
 logging.config.dictConfig(parse_log_config())
 log = logging.getLogger(__name__)
