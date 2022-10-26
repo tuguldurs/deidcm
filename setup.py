8c54d075a8156536410529606e9ff8c887b3182f
tuguldurs <tuguldur.s@gmail.com>
2022-10-26 02:10:55 -0400

    aws cs py

--- setup.py
+++ setup.py
@@ -7,10 +7,7 @@ setup(
     install_requires=[
         'tqdm>=4.62',
         'pydicom==2.3.0',
-        'pyinstaller>=4.8',
-        'gooey>=1.0.8',
-        'names==0.3.0',
         'boto3>=1.24'
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.7.10',
 )
