52b823ce697a9c515e625d7fb1e33c132bdb343d
tuguldurs <tuguldur.s@gmail.com>
2022-10-28 02:25:53 -0400

    import

--- deidcm/__init__.py
+++ deidcm/__init__.py
@@ -4,3 +4,5 @@ from pathlib import Path
 package_path = Path(os.path.dirname(__file__)).parent
 package_config_path = package_path / 'config'
 package_data_path = package_path / 'data'
+
+from .deidentifier import Deidentifier
