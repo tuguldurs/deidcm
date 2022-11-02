9800205b7314ed6f48e278638d4663fc041bc4bd
tuguldurs <tuguldur.s@gmail.com>
2022-11-02 00:54:18 -0400

    dirname fix

--- scripts/testrun.py
+++ scripts/testrun.py
@@ -1,7 +1,7 @@
 import os
 import json
 import shutil
-import logging
+import logging.config
 import argparse
 from pathlib import Path
 
@@ -11,6 +11,11 @@ from collections import namedtuple
 from deidcm.deidentifier import Deidentifier
 from sfredact import SfRedactor
 from deidcm import package_data_path
+from deidcm.utils import parse_log_config
+
+
+logging.config.dictConfig(parse_log_config())
+log = logging.getLogger(__name__)
 
 
 args = namedtuple('args', 'InputDirectory skip_private_tags no_bundled_output')
@@ -22,46 +27,49 @@ def main(in_bucket, out_bucket):
     with open(package_data_path / 'gore.json', 'r') as f:
         studies = json.load(f)
 
-    print(f'this script will process {len(studies)} studies')
+    #studies = [studies[0], studies[-1]]
+
+    log.info(f'this script will process {len(studies)} studies')
 
     for idx, study in enumerate(studies):
-        print(f'processing study - ')
-        print(f'DICOM: {study["DICOM"]}')
-        print(f'   SF: {study["SF"]}')
-        study = study["SF"].split("/")[-1].replace('.pdf', '')
+        log.info(f'processing study - ')
+        log.info(f'DICOM: {study["DICOM"]}')
+        log.info(f'   SF: {study["SF"]}')
+        study_id = study["SF"].split("/")[-1].replace('.pdf', '')
+        log.info(f'study ID = {study_id}')
 
-        print(f'downloading study...')
+        log.info(f'downloading study...')
         in_bucket.download_file(study['DICOM'], 'dcm.zip')
         in_bucket.download_file(study['SF'], 'sf.pdf')
 
-        print('unpacking...')
+        log.info('unpacking...')
         if Path('tmp').is_dir():
             shutil.rmtree('tmp')
         os.mkdir('tmp')
         shutil.unpack_archive('dcm.zip', extract_dir='tmp')
         Path('dcm.zip').unlink()
 
-        print('deidentifying DICOM and SF')
+        log.info('deidentifying DICOM and SF')
         deidentifier.run()
-        redacted_name = f'{study["SF"].split("/")[-1].split(".pdf")[0]}_redacted.pdf'
+        redacted_name = f'{study_id}_redacted.pdf'
         SfRedactor('sf.pdf').redact(redacted_name)
 
-        print('packing deidentified study')
+        log.info('packing deidentified study')
         dirname = [item for item in os.listdir('tmp/deidentified') if '_deidentified' in item][0]
-        shutil.move(f'tmp/deidentified/{dirname}', f'tmp/deidentified/{study}')
-        shutil.make_archive(f'tmp/deidentified/{study}', 'zip', f'tmp/deidentified/{study}')
+        shutil.move(f'tmp/deidentified/{dirname}', f'tmp/deidentified/{study_id}_deidentified')
+        shutil.make_archive(f'tmp/deidentified/{study_id}_deidentified', 'zip', f'tmp/deidentified/{study_id}_deidentified')
 
-        print('uploading...')
-        out_bucket.upload_file(f'tmp/deidentified/{dirname}.zip', f'{dirname}.zip')
+        log.info('uploading...')
+        out_bucket.upload_file(f'tmp/deidentified/{study_id}_deidentified.zip', f'{study_id}_deidentified.zip')
         out_bucket.upload_file(redacted_name, redacted_name)
 
-        print('cleaning...')
+        log.info('cleaning...')
         shutil.rmtree('tmp')
         for fname in os.listdir():
             if fname.endswith('.pdf') or fname.endswith('.zip'):
                 Path(fname).unlink()
 
-        print(f'---> done ({idx+1}/{len(studies)})')
+        log.info(f'---> done ({idx+1}/{len(studies)})')
 
 
 if __name__ == '__main__':
